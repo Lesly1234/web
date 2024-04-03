20a3591c4a370da22f94a051689b2407ef894e7e
Lesly1234 <127535084+Lesly1234@users.noreply.github.com>
2024-04-03 12:43:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(cambiaMensaje, 2000)
+
+function cambiaMensaje() {
+    document.getElementById("loading-message").innerText = "Enviado!"
+}
+
+setTimeout(function () {
+   alert("Recibiste un abrazo y muchos besitos!!! , Hice esto para ti, solo para recordarte lo mucho que me importas je❤️")
+}, 2400)
